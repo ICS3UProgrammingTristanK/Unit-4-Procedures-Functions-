99a74618c14b149b253cf1e5c2b420ea24352d56
Tristan Kalabric <32547188+TristanKalabric@users.noreply.github.com>
2018-11-22 10:48:53 -0500

    created percentage program

---
+++ PercentTristanK/PercentTristanK/PercentForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PercentTristanK
+{
+    partial class frmPercent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnterGrade = new System.Windows.Forms.Label();
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEnterGrade
+            // 
+            this.lblEnterGrade.AutoSize = true;
+            this.lblEnterGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterGrade.Location = new System.Drawing.Point(12, 24);
+            this.lblEnterGrade.Name = "lblEnterGrade";
+            this.lblEnterGrade.Size = new System.Drawing.Size(145, 20);
+            this.lblEnterGrade.TabIndex = 0;
+            this.lblEnterGrade.Text = "Enter a grade level:";
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Location = new System.Drawing.Point(163, 26);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(100, 20);
+            this.txtGrade.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(90, 68);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(105, 35);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmPercent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 130);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.lblEnterGrade);
+            this.Name = "frmPercent";
+            this.Text = "Percentage Program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnterGrade;
+        private System.Windows.Forms.TextBox txtGrade;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ PercentTristanK/PercentTristanK/PercentForm.cs
@@ -0,0 +1,113 @@
+﻿/*
+ * Created by: Tristan Kalabric
+ * Created on: November 22, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #33 - Percentage Program
+ * This program converts the users grade level to a percent and displays it in a messagebox
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PercentTristanK
+{
+    public partial class frmPercent : Form
+    {
+        public frmPercent()
+        {
+            InitializeComponent();
+        }
+
+        private int ConvertPercent(String level)
+        {
+            int percentage;
+
+            switch (level)
+            {
+                case "4++":
+                    percentage = 98;
+                    break;
+                case "4+":
+                    percentage = 95;
+                    break;
+                case "4":
+                    percentage = 85;
+                    break;
+                case "4-":
+                    percentage = 80;
+                    break;
+                case "3+":
+                    percentage = 78;
+                    break;
+                case "3":
+                    percentage = 75;
+                    break;
+                case "3-":
+                    percentage = 72;
+                    break;
+                case "2+":
+                    percentage = 68;
+                    break;
+                case "2":
+                    percentage = 65;
+                    break;
+                case "2-":
+                    percentage = 62;
+                    break;
+                case "1+":
+                    percentage = 58;
+                    break;
+                case "1":
+                    percentage = 55;
+                    break;
+                case "1-":
+                    percentage = 52;
+                    break;
+                case "0+":
+                    percentage = 45;
+                    break;
+                case "0":
+                    percentage = 40;
+                    break;
+                case "0-":
+                    percentage = 35;
+                    break;
+                case "0--":
+                    percentage = 30;
+                    break;
+                case "0---":
+                    percentage = 20;
+                    break;
+                case "0----":
+                    percentage = 10;
+                    break;
+                default:
+                    percentage = -1;
+                    break;
+            }
+
+            return percentage;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Declare variables
+            String userLevel;
+            int userPercentage;
+
+            //Get the users grade and percentage
+            userLevel = this.txtGrade.Text;
+            userPercentage = this.ConvertPercent(userLevel);
+
+            //Display messagebox
+            MessageBox.Show("Level " + userLevel + " is equal to: " + userPercentage + "%");
+        }
+    }
+}
