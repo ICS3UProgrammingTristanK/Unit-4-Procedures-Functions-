d4db22765101c8b4ae0628a1fd4ccb3b46a68ab4
Tristan Kalabric <32547188+TristanKalabric@users.noreply.github.com>
2018-11-26 10:08:02 -0500

    Created Dynamic Picturebox Program

---
+++ DynamicPicTristanK/DynamicPicTristanK/DynamicPicForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace DynamicPicTristanK
+{
+    partial class frmDynamicPic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblClickOnPic = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblClickOnPic
+            // 
+            this.lblClickOnPic.AutoSize = true;
+            this.lblClickOnPic.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClickOnPic.Location = new System.Drawing.Point(199, 9);
+            this.lblClickOnPic.Name = "lblClickOnPic";
+            this.lblClickOnPic.Size = new System.Drawing.Size(398, 25);
+            this.lblClickOnPic.TabIndex = 0;
+            this.lblClickOnPic.Text = "Click on a picture to make the man walk.";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(332, 241);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(86, 36);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.Image = global::DynamicPicTristanK.Properties.Resources.walk10;
+            this.PictureBox.Location = new System.Drawing.Point(577, 241);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(100, 115);
+            this.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBox.TabIndex = 2;
+            this.PictureBox.TabStop = false;
+            this.PictureBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // frmDynamicPic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 562);
+            this.Controls.Add(this.PictureBox);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblClickOnPic);
+            this.Name = "frmDynamicPic";
+            this.Text = "Dynamic Pictureboxes";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblClickOnPic;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox PictureBox;
+    }
+}
+
---
+++ DynamicPicTristanK/DynamicPicTristanK/DynamicPicForm.cs
@@ -0,0 +1,142 @@
+﻿/*
+ * Created by: Tristan Kalabric
+ * Created on: November 23, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #32 - Name of Program
+ * This program lets the user click on a picture box and make it seem like the man is moving
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace DynamicPicTristanK
+{
+    public partial class frmDynamicPic : Form
+    {
+
+        private void GeneratePictureBoxes()
+        {
+            //Generate each of the picture boxes on the form
+            GeneratePictureBox(61, 78);
+            GeneratePictureBox(464, 78);
+            GeneratePictureBox(61, 358);
+            GeneratePictureBox(464, 358);
+
+        }
+
+        private void GeneratePictureBox(int x, int y)
+        {
+            //Dynamically generate a new picture box and a new point at the given location(x,y)
+            PictureBox tmpPicMan = new PictureBox();
+            Point pntPic = new System.Drawing.Point(x, y);
+
+            //Set the location of the picture box
+            tmpPicMan.Location = pntPic;
+
+            //Assign the image to the picture box
+            tmpPicMan.Image = Properties.Resources.walk1;
+
+            //Stretch the image to the size of the picture box
+            tmpPicMan.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            //Make the picture box width and height to be the same a the image
+            tmpPicMan.ClientSize = new Size(Properties.Resources.walk1.Width, Properties.Resources.walk1.Height);
+
+            //Add the event listener
+            tmpPicMan.Click += new System.EventHandler(pictureBox1_Click);
+
+            //Add the picture box to the form
+            this.Controls.Add(tmpPicMan);
+        }
+
+
+
+        public frmDynamicPic()
+        {
+            InitializeComponent();
+            lblClickOnPic.Hide();
+            PictureBox.Hide();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Display label
+            lblClickOnPic.Show();
+
+            //Generate the picture boxes with their event listeners
+            GeneratePictureBoxes();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //Cast the sender object into a picture box
+            PictureBox tmpPicMan = (PictureBox)sender;
+
+            //Write a message to the console to check which picture box was clicked
+            Console.WriteLine("Picture box (" + tmpPicMan.Location.X + ", " + tmpPicMan.Location.Y + ") was clicked.");
+
+            //Declare local variables and constants
+            const byte MAX_FRAMES = 10;
+            int pictureFrameCounter = 1;
+
+            //Continue this loop while the frame counter has not reached the max number of frames we have to display
+            while (pictureFrameCounter < MAX_FRAMES + 1)
+            {
+                if (pictureFrameCounter == 1)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk1;
+                }
+                else if (pictureFrameCounter == 2)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk2;
+                }
+                else if (pictureFrameCounter == 3)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk3;
+                }
+                else if (pictureFrameCounter == 4)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk4;
+                }
+                else if (pictureFrameCounter == 5)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk5;
+                }
+                else if (pictureFrameCounter == 6)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk6;
+                }
+                else if (pictureFrameCounter == 7)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk7;
+                }
+                else if (pictureFrameCounter == 8)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk8;
+                }
+                else if (pictureFrameCounter == 9)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk9;
+                }
+                else
+                {
+                    tmpPicMan.Image = Properties.Resources.walk10;
+                }
+                //Incement counter by 1
+                pictureFrameCounter = pictureFrameCounter + 1;
+                this.Refresh();
+
+                //Pause the loop for 100 milliseconds
+                Thread.Sleep(100);
+            }
+        }
+    }
+    }
+
